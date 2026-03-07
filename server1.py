b579b63254f155593252fc5f785052b7ee516b2c
Hayden Burker <hayden.burker@gmail.com>
2026-03-06 22:51:36 -0700

    creates webhook and message route

--- server1.py
+++ server1.py
@@ -1,8 +1,18 @@
 import os
+import requests
 from flask import Flask
 
 FLASK_PORT = os.environ.get("SERVER1_PORT")
+SERVER2_URL = os.environ.get("SERVER2_URL")
 app = Flask(__name__)
+
+@app.route("/send-message", methods=["POST"])
+def send_message():
+    response = requests.post(f"{SERVER2_URL}/webhook", json={"message": "Hello from server 1!"})
+    if response.status_code == 200:
+        return "message sent", 200
+    else:
+        return "failed to send message", response.status_code
 
 if __name__ == "__main__":
     app.run(port=FLASK_PORT)
--- server2.py
+++ server2.py
@@ -1,8 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 FLASK_PORT = os.environ.get("SERVER2_PORT")
 app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    print("webhook event: ", request.get_json())
+    return "success", 200
 
 if __name__ == "__main__":
     app.run(port=FLASK_PORT)
